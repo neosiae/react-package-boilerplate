60561169251decdaa14acfd474d04081e5fe643b
Denis Kotlica <neosiae@gmail.com>
2018-12-20 20:34:46 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# react-on-intersect 
+
+## Demo
+
 # react-package-boilerplate
+
+## Installation
+
+## Usage
+
+## API
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ example/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Example () {
+  return (
+    <h1>Example</h1>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Example />)
